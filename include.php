d887bf1c08984398c365cbd4918200ddd01e7931
ProklUng <funtik21975@gmail.com>
2021-07-25 19:45:03 +0300

    Fixes

--- include.php
+++ include.php
@@ -1,49 +1,13 @@
 <?php
 
 use Bitrix\Main\Loader;
-use ProklUng\Module\Boilerplate\Options\ModuleManager;
-use Proklung\Symfony\Router\Subscribers\OnAfterSaveOptionsHandler;
-use Proklung\Symfony\Router\Subscribers\ValidatorNativeConfigs;
-use Proklung\Symfony\Router\Subscribers\ValidatorSymfonyConfigs;
+use Proklung\Symfony\Router\Utils\Bootstrap;
 
-Loader::registerAutoLoadClasses(
-    'proklung.symfony.router',
+Loader::registerAutoLoadClasses('proklung.symfony.router',
     [
         'proklung_symfony_router' => 'install/index.php',
     ]
 );
 
-$moduleManager = new ModuleManager('proklung.symfony.router');
-$configFilePath = $moduleManager->get('yaml_config_file_path');
-$cachePath =  $moduleManager->get('yaml_cache_path');
-
-\Bitrix\Main\EventManager::getInstance()->addEventHandler(
-    $moduleManager->getModuleId(),
-    'OnAfterSaveOptions',
-    [new OnAfterSaveOptionsHandler, 'handler']
-);
-
-\Bitrix\Main\EventManager::getInstance()->addEventHandler(
-    $moduleManager->getModuleId(),
-    'OnBeforeSetOption',
-    [new ValidatorNativeConfigs, 'handler']
-);
-
-\Bitrix\Main\EventManager::getInstance()->addEventHandler(
-    $moduleManager->getModuleId(),
-    'OnBeforeSetOption',
-    [new ValidatorSymfonyConfigs, 'handler']
-);
-
-$routerInstance = \Proklung\Symfony\Router\Utils\Loader::from($configFilePath, $cachePath);
-
-if (Proklung\Symfony\Router\Utils\Loader::checkRequirements()) {
-    $configBitrixRoutesPath = $moduleManager->get('native_config_file_path');
-    $cacheBitrixRoutesPath =  $moduleManager->get('native_yaml_cache_path');
-    $phpConfigFile =  $moduleManager->get('php_router_config_path');
-
-    $routeConvertor = Proklung\Symfony\Router\Utils\Loader::native($configBitrixRoutesPath, $cacheBitrixRoutesPath);
-    if ($routeConvertor !== null) {
-        Proklung\Symfony\Router\Utils\Loader::save($phpConfigFile, $routeConvertor);
-    }
-}
+$bootstrapModule = new Bootstrap();
+$bootstrapModule->init();
